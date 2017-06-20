4837c42067f22198e4ebce6ea507c6155273e917
funnystyle <esero@hanmail.net>
2017-06-20 17:34:48 +0900

    093 duplicate numbers

---
+++ answers/093 duplicate numbers.js
@@ -0,0 +1,20 @@
+/*
+Duplicate Numbers
+
+일전에 뭐 게임 회사에서 본 간단한 퀴즈 테스트 입니다.
+
+0~9까지의 문자로 된 숫자를 입력 받았을 때, 이 입력 값이 0~9까지의 숫자가 각각 한 번 씩만 사용된 것인지 확인하는 함수를 구하시오.
+
+sample inputs: 0123456789 01234 01234567890 6789012345 012322456789
+sample outputs: true false false true false
+no duplicates
+*/
+
+
+var once = str => str.split("").sort().join("") === "0123456789";
+
+var input = ["0123456789", "01234", "01234567890", "6789012345", "012322456789"];
+
+for (s of input) {
+    console.log(`${s} : ${once(s)}`);
+}
