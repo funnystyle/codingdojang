f251e8e4adc494472f1915eddf6872812d6acaff
funnystyle <esero@hanmail.net>
2017-06-21 03:17:33 +0900

    090 pothole_case

--- answers/090 pothole_case.js
+++ answers/090 pothole_case.js
@@ -14,11 +14,28 @@ numGoat30 --> num_goat_3_0
 출처: UT past test
 */
 
+/*
+codingDojang --> coding_dojang
+
+numGoat30 --> num_goat_3_0
+*/
+
 // regular expression 이용
 // 첫 글자가 대문자인 경우 소문자로는 바꿔주나 _는 앞에 붙이지 말아야 한다.
 // 따라서 첫 글자 처리는 따로 함
 var pothole_case = function(camel) {
     return (camel[0] || "").toLowerCase() + camel.slice(1).replace(/([A-Z0-9])/g, (s => `_${s.toLowerCase()}`));
+};
+
+console.log(pothole_case("codingDojang"));
+console.log(pothole_case("CodingDojang"));
+console.log(pothole_case("numGoat30"));
+console.log(pothole_case(""));
+
+
+// reduce 이용
+var pothole_case = function(camel) {
+    return camel.split("").reduce((a, b) => a + b.replace(/([A-Z0-9])/g, (s => `_${s.toLowerCase()}`)), "");
 };
 
 console.log(pothole_case("codingDojang"));
